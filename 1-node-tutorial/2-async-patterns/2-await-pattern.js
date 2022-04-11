9361501618105cf0d1685b58505440c01c19c2d0
Leonerdo15 <leonardolage10@gmail.com>
2022-04-11 19:55:39 +0100

    express

--- 1-node-tutorial/2-async-patterns/2-await-pattern.js
+++ 1-node-tutorial/2-async-patterns/2-await-pattern.js
@@ -42,41 +42,41 @@ start()
  *
  * */
 
-const {readFile, writeFile} = require('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
-
-
-
-
-const start = async() => {
-  try{
-    const first = await readFile('./content/first.txt', 'utf8')
-    const second = await readFile('./content/second.txt','utf8')
-    await writeFile('./content/result-14',
-        `This is awesome: ${first} ${second}`,
-        {flag: 'a'})
-    console.log(first, second)
-  } catch (error){
-    console.log(error)
-  }
-}
-
-start()
-
-// const getText = (path) =>{
-//     return new Promise((resolve, reject)=> {
-//         readFile(path, (err, data)=> {
-//             if (err){
-//                 reject(err)
-//             } else {
-//                 resolve(data)
-//             }
-//         })
-//     })
+// const {readFile, writeFile} = require('fs').promises
+// // const util = require('util')
+// // const readFilePromise = util.promisify(readFile)
+// // const writeFilePromise = util.promisify(writeFile)
+//
+//
+//
+//
+// const start = async() => {
+//   try{
+//     const first = await readFile('./content/first.txt', 'utf8')
+//     const second = await readFile('./content/second.txt','utf8')
+//     await writeFile('./content/result-14',
+//         `This is awesome: ${first} ${second}`,
+//         {flag: 'a'})
+//     console.log(first, second)
+//   } catch (error){
+//     console.log(error)
+//   }
 // }
-
-// getText('./content/first.txt')
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
+//
+// start()
+//
+// // const getText = (path) =>{
+// //     return new Promise((resolve, reject)=> {
+// //         readFile(path, (err, data)=> {
+// //             if (err){
+// //                 reject(err)
+// //             } else {
+// //                 resolve(data)
+// //             }
+// //         })
+// //     })
+// // }
+//
+// // getText('./content/first.txt')
+// //     .then((result) => console.log(result))
+// //     .catch((err) => console.log(err))

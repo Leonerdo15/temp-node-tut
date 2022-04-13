49dc51452fa9896ddccbccc08ebc0bd692533933
Leonerdo15 <leonardolage10@gmail.com>
2022-04-13 12:13:49 +0100

    express

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,12 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.send('Home page')
+})
+
+app.get('/about')
+
+app.listen(5000, () =>{
+    console.log('Server is listening on port 5000')
+})
